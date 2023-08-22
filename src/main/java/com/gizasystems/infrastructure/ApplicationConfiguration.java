d5cb1e0fc26e2cd23254153d18de6960fc7df840
Ahmed Yassin <yassinahmed670@gmail.com>
2023-08-22 17:11:01 +0300

    Initial commit

---
+++ src/main/java/com/gizasystems/infrastructure/ApplicationConfiguration.java
@@ -0,0 +1,16 @@
+package com.gizasystems.infrastructure;
+
+import com.gizasystems.observability.PerformanceHandler;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+    @Bean
+    public ObservedAspect observedAspect(ObservationRegistry observationRegistry){
+        observationRegistry.observationConfig().observationHandler(new PerformanceHandler());
+        return new ObservedAspect(observationRegistry);
+    }
+}
